a6b8ce545d514e30efbea5ae450780f4020a0534
Jeferson Andres Aguirre Moreno <jefersonaaguirremoreno@gmail.com>
2025-11-09 11:05:02 -0500

    Segunda version: se muestra la longitud del nombre

--- hola.py
+++ hola.py
@@ -1,5 +1,7 @@
 # hola.py
+# Version mejorada
 # Autor: Jeferson Andres Aguirre Moreno
 # Descripcion : Script basico que saluda al usuario
 nombre = input('¿Cual es tu nombre?: ')
 print(f'!HOLA, {nombre}¡ Bienvebido a tu primer proyecto con Git y Python')
+print(f'Tu nombre tiene {len(nombre)} letras. ')
